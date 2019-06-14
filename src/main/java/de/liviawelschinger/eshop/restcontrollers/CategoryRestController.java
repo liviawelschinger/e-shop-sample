7a9dfbc5e73739b150c1c53797610f40e92cf77e
livia_welschinger <livia-welschinger@t-online.de>
2019-06-14 17:53:41 +0200

    Added findById endpoints

--- src/main/java/de/liviawelschinger/eshop/restcontrollers/CategoryRestController.java
+++ src/main/java/de/liviawelschinger/eshop/restcontrollers/CategoryRestController.java
@@ -4,10 +4,12 @@ import de.liviawelschinger.eshop.entities.Category;
 import de.liviawelschinger.eshop.entities.Product;
 import de.liviawelschinger.eshop.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/api/rest")
@@ -20,6 +22,12 @@ public class CategoryRestController {
     @RequestMapping(value = "/categories", produces = "application/json", method = RequestMethod.GET)
     public Flux<Category> findAllCategory() {
         return categoryRepository.findAll();
+    }
+
+    // http://localhost:8085/api/rest/category/id
+    @RequestMapping(value = "/category/{id}", produces = "application/json", method = RequestMethod.GET)
+    public Mono<Category> findById(@PathVariable(name = "id") String id) {
+        return  categoryRepository.findById(id);
     }
 
 
--- src/main/java/de/liviawelschinger/eshop/restcontrollers/ProductRestController.java
+++ src/main/java/de/liviawelschinger/eshop/restcontrollers/ProductRestController.java
@@ -3,10 +3,12 @@ package de.liviawelschinger.eshop.restcontrollers;
 import de.liviawelschinger.eshop.entities.Product;
 import de.liviawelschinger.eshop.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/api/rest")
@@ -19,5 +21,11 @@ public class ProductRestController {
     @RequestMapping(value = "/products", produces = "application/json", method = RequestMethod.GET)
     public Flux<Product> findAllProducts() {
         return productRepository.findAll();
+    }
+
+    // http://localhost:8085/api/rest/product/id
+    @RequestMapping(value = "/product/{id}", produces = "application/json", method = RequestMethod.GET)
+    public Mono<Product> findById(@PathVariable(name = "id") String id) {
+        return productRepository.findById(id);
     }
 }
